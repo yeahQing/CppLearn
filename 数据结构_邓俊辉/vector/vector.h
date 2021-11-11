66d33942b015868850e23ba74e227f684ddc690c
yeahQing <joashzhai@outlook.com>
2021-11-11 23:44:16 +0800

    todo: add vector header file

---
+++ 数据结构_邓俊辉/vector/vector.h
@@ -0,0 +1,22 @@
+#ifndef __VECTOR_H__
+#define __VECTOR_H__
+
+typedef int Rank;
+
+#define DEFAULT_CAPACITY 3
+
+template <typename T>
+class Vector {
+protected:
+    Rank _size;
+    int _capacity;
+    T* _elem;
+	// TODO
+public:
+    // TODO
+    // 构造函数
+    
+};
+
+
+#endif // __VECTOR_H__
